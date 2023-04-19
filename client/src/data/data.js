d7bee46c132a679b42ba4281688fbb9d491d8f7a
Victor Ajagunna <victor-ajagunna@hotmail.com>
2023-04-19 10:16:25 +0200

    defined data outputs

--- client/src/data/data.js
+++ client/src/data/data.js
@@ -1,4 +1,40 @@
 const data = [
-    {id , status: "", title: "", content: "",},
+  {
+    id: 1,
+    status: "open",
+    title: "Build web portfolio",
+    content: "Build your online presence",
+  },
+  {
+    id: 2,
+    status: "open",
+    title: "Price list",
+    content: "Price list for your jobs",
+  },
+  {
+    id: 3,
+    status: "open",
+    title: "Article",
+    content: "Building smart mobile Apps",
+  },
+  {
+    id: 4,
+    status: "open",
+    title: "Review and Debugging",
+    content: "Website Role based Authentications",
+  },
+  {
+    id: 5,
+    status: "open",
+    title: "Networking",
+    content: "Working alone or with others",
+  },
 ];
-const statuses = [];
+const statuses = [
+  { index: 1, status: "open", color: "#EB5A46" },
+  { index: 2, status: "in progress", color: "#00C2E0" },
+  { index: 3, status: "in review", color: "#C377E0" },
+  { index: 4, status: "done", color: "#3981DE" },
+];
+
+export { data, statuses };
